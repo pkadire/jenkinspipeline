ee32f64ba386f6841f9a39a2fa63c51c8b4ea6f8
pkadire <prashanthkadire123@gmail.com>
2023-02-11 13:56:28 -0600

    first commit

---
+++ src/main/java/com/example/jenkins/jenkinspipeline/JenkinspipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinspipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinspipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/jenkinspipeline/JenkinspipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinspipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
